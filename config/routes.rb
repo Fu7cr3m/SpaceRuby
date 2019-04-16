05828e9e7dabe22969d1052e3f526f692b794c96
Fu7cr3m <dmf.diego@gmail.com>
2019-04-16 02:01:49 -0300

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+root 'planets#index'
   resources :powers
   resources :planets
   resources :aliens
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#Rails.application.routes.draw do
+ # root 'cars#index'
+#  resources :cars
+#end
